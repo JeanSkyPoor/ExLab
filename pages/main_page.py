4bbe3f850780c14589d365d8e55e23bdf3c89b29
Jean_Sky_Poor <katovano2705@gmail.com>
2022-11-30 14:21:12 +0300

    little beauty

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,8 @@
 from pages.base_page import BasePage
 import allure
 
+from pages.locators import MainPageLocators
+
 
 class MainPage(BasePage):
 
@@ -9,6 +11,7 @@ class MainPage(BasePage):
 
         theme_type: there are two options: 'dark' and 'white'. 
         """
+
         with allure.step("Checking background color..."):            
             color = self.get_background_color()
             if theme_type == 'dark':
@@ -17,41 +20,46 @@ class MainPage(BasePage):
                 assert color == '#ffffff', f"Theme's color is not white, have to be #ffffff, but have {color}"
 
 
-    def check_item_src(self, correct_src: str, item_locator: tuple) -> None:
+    def matching_item_attribute(self, correct_src: str, attribute: str, locator: tuple) -> None:
         """Matching correct_src and current src from element by 'webelement.get_attribute(src)
 
-        correct_src: str like 'http://test.exlab.team/gif/logo.gif'
-        item_locator: tuple like (By.CSS_SECELTOR, 'random selector'). Not unzipped 
+        correct_src: str like 'http://test.exlab.team/gif/logo.gif'\n
+        attribute: str like 'src' \n
+        locator: tuple like (By.CSS_SECELTOR, 'random selector'). Not unzipped         
         '"""
-        with allure.step("Checking src from item..."):
-            item_src = self.browser.find_element(*item_locator).get_attribute('src')
-            assert item_src == correct_src, f"SRC is not correct. Have to be {correct_src}, but have {item_src}"
+
+        with allure.step(f"Checking item's {attribute} ..."):
+            srs = self.get_element_attribute(locator, attribute)
+            assert srs == correct_src, f"SRC is not correct. Have to be {correct_src}, but have {srs}"
 
 
     def check_element_is_present(self, button_name: str, button_locator: tuple) -> None:
-        """
-        Checking if an element exists on the page
+        """Checking if an element exists on the page
 
-        button_name: how you wanna see button's name on report and assert error
+        button_name: how you wanna see button's name on report and assert error \n
         button_locator: tuple like (By.CSS_SECELTOR, 'random selector'). Not unzipped 
         """
+
         with allure.step(f"Checking {button_name} on page..."):
             assert self.is_element_present(*button_locator), f"{button_name} is not founded"
 
     
     def check_anchor_element_after_shifting(self, anchor_element_name: str, anchor_locator: tuple) -> None:
-        """
-        Checking anchor element after shifting
+        """Checking anchor element after shifting
 
-        anchor_element_name: how you wanna see anchor's name on report and assert error
+        anchor_element_name: how you wanna see anchor's name on report and assert error \n
         anchor_locator: tuple like (By.CSS_SECELTOR, 'random selector'). Not unzipped 
         """
+
         with allure.step(f"Checking {anchor_element_name} after shifting..."):
             assert self.checking_visibility_of_element_located(*anchor_locator), f"{anchor_element_name} anchor is not founded after shifting"
             
 
     def check_join_opened_correct_url(self, currect_url: str) -> None:
         """Switch to the last window handless and check current URL and currect URL"""
+
         with allure.step(f"Checking url, have to be... {currect_url}"):
             self.browser.switch_to.window(self.browser.window_handles[-1])
             assert self.checking_url_to_be(currect_url), f'URL is not correct. Have to be {currect_url}, but {self.browser.current_url}'
+
+    
