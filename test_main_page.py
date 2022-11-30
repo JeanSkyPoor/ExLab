980a5c28f56abe554de71a4f155d72fff66f2305
Jean_Sky_Poor <katovano2705@gmail.com>
2022-11-30 12:16:40 +0300

    update def's name

--- test_main_page.py
+++ test_main_page.py
@@ -20,7 +20,7 @@ def test_check_about_us_header(browser):
     """Checking About Us button and shift to About Us block. [3] test"""
     page = MainPage(browser, MainPageLocators.LINK)
     page.open()
-    page.check_item_is_present('About Us', MainPageLocators.ABOUT_US_BUTTON_HEADER)    
+    page.check_element_is_present('About Us', MainPageLocators.ABOUT_US_BUTTON_HEADER)    
     page.click_on_button(*MainPageLocators.ABOUT_US_BUTTON_HEADER)
     page.check_anchor_element_after_shifting('About Us', MainPageLocators.ABOUT_US_ANCHOR)
 
@@ -29,7 +29,7 @@ def test_check_project_header(browser):
     """Checking Project button and shift to Project block. [4] test"""
     page = MainPage(browser, MainPageLocators.LINK)
     page.open()
-    page.check_item_is_present('Project', MainPageLocators.PROJECTS_BUTTON_HEADER)
+    page.check_element_is_present('Project', MainPageLocators.PROJECTS_BUTTON_HEADER)
     page.click_on_button(*MainPageLocators.PROJECTS_BUTTON_HEADER)
     page.check_anchor_element_after_shifting('Project', MainPageLocators.PROJECT_ANCHOR)
 
@@ -38,7 +38,7 @@ def test_check_mentors_header(browser):
     """Checking Mentors button and shift to Mentors block. [5] test"""
     page = MainPage(browser, MainPageLocators.LINK)
     page.open()
-    page.check_item_is_present('Mentors', MainPageLocators.MENTORS_BUTTON_HEADER)
+    page.check_element_is_present('Mentors', MainPageLocators.MENTORS_BUTTON_HEADER)
     page.click_on_button(*MainPageLocators.MENTORS_BUTTON_HEADER)
     page.check_anchor_element_after_shifting('Mentors', MainPageLocators.MENTORS_ANCHOR)
 
@@ -47,7 +47,7 @@ def test_check_startup_for_header(browser):
     """Checking StartUp For button and shift to StartUp For block. [6] test"""
     page = MainPage(browser, MainPageLocators.LINK)
     page.open()
-    page.check_item_is_present('StartUp For', MainPageLocators.STARTUP_FOR_BUTTON_HEADER)
+    page.check_element_is_present('StartUp For', MainPageLocators.STARTUP_FOR_BUTTON_HEADER)
     page.click_on_button(*MainPageLocators.STARTUP_FOR_BUTTON_HEADER)
     page.check_anchor_element_after_shifting('StartUp For', MainPageLocators.STARTUP_FOR_ANCHOR)
 
@@ -56,7 +56,7 @@ def test_click_sun_icon(browser):
     """Checking Sun Icon and theme's color. Have to be white. [7] test"""
     page = MainPage(browser, MainPageLocators.LINK)
     page.open()
-    page.check_item_is_present('Sun Icon', MainPageLocators.SUN_ICON)
+    page.check_element_is_present('Sun Icon', MainPageLocators.SUN_ICON)
     page.click_on_button(*MainPageLocators.SUN_ICON)
     page.check_background_color('white')
 
@@ -65,6 +65,6 @@ def test_check_join_header(browser):
     """Checking Join button open registration TG-bot. [9] test"""
     page = MainPage(browser, MainPageLocators.LINK)
     page.open()
-    page.check_item_is_present('Join', MainPageLocators.JOIN_BUTTON_HEADER)
+    page.check_element_is_present('Join', MainPageLocators.JOIN_BUTTON_HEADER)
     page.click_on_button(*MainPageLocators.JOIN_BUTTON_HEADER)
     page.check_join_opened_correct_url('https://t.me/ExLab_registration_bot')
