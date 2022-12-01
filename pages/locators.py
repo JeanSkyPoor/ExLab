15838ec782378443e7d1486a7e42db2230b73db1
Jean_Sky_Poor <katovano2705@gmail.com>
2022-12-01 18:07:35 +0300

    update locators after refactoring

--- pages/locators.py
+++ pages/locators.py
@@ -3,33 +3,55 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     
     LINK = 'http://test.exlab.team/'
-    BACKGROUND_FOR_THEME = (By.CSS_SELECTOR, '#root > div')
-
-    #Block HEADERS
-    LIGHT_ICON_WHILE_DARK_MODE = (By.CSS_SELECTOR, 'div.sc-fnykZs.fEkGUM')
-    LOGO_ICON = (By.CSS_SELECTOR, 'div.sc-gKXOVf >div.sc-jqUVSM.EnPDN')
-    ABOUT_US_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(1)')
-    PROJECTS_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(2)')
-    MENTORS_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(3)')
-    STARTUP_FOR_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(4)')
-    JOIN_BUTTON_HEADER = (By.CSS_SELECTOR, 'div.sc-hAZoDl.hrEelO')
-    SUN_ICON = (By.CSS_SELECTOR, 'div.sc-fnykZs.fEkGUM')
     
-    #Anchor to blocks
-    ABOUT_US_ANCHOR = (By.CSS_SELECTOR, '#about')
-    PROJECT_ANCHOR = (By.CSS_SELECTOR, '#projects')
-    MENTORS_ANCHOR = (By.CSS_SELECTOR, '#mentors')
-    STARTUP_FOR_ANCHOR = (By.CSS_SELECTOR, '#startup')
-    WHY_EXLAB_ANCHOR = (By.CSS_SELECTOR, 'div.sc-jdAMXn.iDQeOI')
+    THEME = (By.CSS_SELECTOR, 'div.sc-bczRLJ')
 
-    #Block YOUR OPPORTUNITY
-    LOGO_GIF = (By.CSS_SELECTOR, 'div.sc-idiyUo.jKIKBm > img')
-    YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-kgflAQ.gupdxc')
-    TEXT_UNDER_YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-bBrHrO.lmeoyY')
+    #Block HEADER
 
-    #Block ABOUT US
-    ABOUT_US_MAIN = (By.CSS_SELECTOR, '#about > div.sc-eCYdqJ.koNCEH.is-inview')
-    ABOUT_US_BLOCK_TEXT = (By.CSS_SELECTOR, 'p.sc-cCsOjp.cdaqyF')
-    WHY_EXLAB_MAIN = (By.CSS_SELECTOR, 'div.sc-bZnhIo.CLhmH.is-inview')
-    WHY_EXLAB_BLOCK_TEXT = (By.CSS_SELECTOR, 'ol.sc-iAvgwm.fQGFrP.is-inview')
-    WHY_EXLAB_JOIN = (By.CSS_SELECTOR, 'a.sc-dkzDqf.gpYSxm.is-inview')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # BACKGROUND_FOR_THEME = (By.CSS_SELECTOR, '#root > div')
+
+    # #Block HEADERS
+    # LIGHT_ICON_WHILE_DARK_MODE = (By.CSS_SELECTOR, 'div.sc-fnykZs.fEkGUM')
+    # LOGO_ICON = (By.CSS_SELECTOR, 'div.sc-gKXOVf >div.sc-jqUVSM.EnPDN')
+    # ABOUT_US_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(1)')
+    # PROJECTS_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(2)')
+    # MENTORS_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(3)')
+    # STARTUP_FOR_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(4)')
+    # JOIN_BUTTON_HEADER = (By.CSS_SELECTOR, 'div.sc-hAZoDl.hrEelO')
+    # SUN_ICON = (By.CSS_SELECTOR, 'div.sc-fnykZs.fEkGUM')
+    
+    # #Anchor to blocks
+    # ABOUT_US_ANCHOR = (By.CSS_SELECTOR, '#about')
+    # PROJECT_ANCHOR = (By.CSS_SELECTOR, '#projects')
+    # MENTORS_ANCHOR = (By.CSS_SELECTOR, '#mentors')
+    # STARTUP_FOR_ANCHOR = (By.CSS_SELECTOR, '#startup')
+    # WHY_EXLAB_ANCHOR = (By.CSS_SELECTOR, 'div.sc-jdAMXn.iDQeOI')
+
+    # #Block YOUR OPPORTUNITY
+    # LOGO_GIF = (By.CSS_SELECTOR, 'div.sc-idiyUo.jKIKBm > img')
+    # YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-kgflAQ.gupdxc')
+    # TEXT_UNDER_YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-bBrHrO.lmeoyY')
+
+    # #Block ABOUT US
+    # ABOUT_US_MAIN = (By.CSS_SELECTOR, '#about > div.sc-eCYdqJ.koNCEH.is-inview')
+    # ABOUT_US_BLOCK_TEXT = (By.CSS_SELECTOR, 'p.sc-cCsOjp.cdaqyF')
+    # WHY_EXLAB_MAIN = (By.CSS_SELECTOR, 'div.sc-bZnhIo.CLhmH.is-inview')
+    # WHY_EXLAB_BLOCK_TEXT = (By.CSS_SELECTOR, 'ol.sc-iAvgwm.fQGFrP.is-inview')
+    # WHY_EXLAB_JOIN = (By.CSS_SELECTOR, 'a.sc-dkzDqf.gpYSxm.is-inview')
