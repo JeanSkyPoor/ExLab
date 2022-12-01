e61855c03019274a7a85dd9a3537000f4f57c223
Jean_Sky_Poor <katovano2705@gmail.com>
2022-12-01 15:33:42 +0300

    add locators for About Us block

--- pages/locators.py
+++ pages/locators.py
@@ -15,15 +15,21 @@ class MainPageLocators():
     JOIN_BUTTON_HEADER = (By.CSS_SELECTOR, 'div.sc-hAZoDl.hrEelO')
     SUN_ICON = (By.CSS_SELECTOR, 'div.sc-fnykZs.fEkGUM')
     
-
+    #Anchor to blocks
     ABOUT_US_ANCHOR = (By.CSS_SELECTOR, '#about')
     PROJECT_ANCHOR = (By.CSS_SELECTOR, '#projects')
     MENTORS_ANCHOR = (By.CSS_SELECTOR, '#mentors')
     STARTUP_FOR_ANCHOR = (By.CSS_SELECTOR, '#startup')
-
-    
+    WHY_EXLAB_ANCHOR = (By.CSS_SELECTOR, 'div.sc-jdAMXn.iDQeOI')
 
     #Block YOUR OPPORTUNITY
     LOGO_GIF = (By.CSS_SELECTOR, 'div.sc-idiyUo.jKIKBm > img')
     YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-kgflAQ.gupdxc')
     TEXT_UNDER_YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-bBrHrO.lmeoyY')
+
+    #Block ABOUT US
+    ABOUT_US_MAIN = (By.CSS_SELECTOR, '#about > div.sc-eCYdqJ.koNCEH.is-inview')
+    ABOUT_US_BLOCK_TEXT = (By.CSS_SELECTOR, 'p.sc-cCsOjp.cdaqyF')
+    WHY_EXLAB_MAIN = (By.CSS_SELECTOR, 'div.sc-bZnhIo.CLhmH.is-inview')
+    WHY_EXLAB_BLOCK_TEXT = (By.CSS_SELECTOR, 'ol.sc-iAvgwm.fQGFrP.is-inview')
+    WHY_EXLAB_JOIN = (By.CSS_SELECTOR, 'a.sc-dkzDqf.gpYSxm.is-inview')
