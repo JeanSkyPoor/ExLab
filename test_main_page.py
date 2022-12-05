5a347154eb6deeb752cf88d24e89a2c8623e6133
Jean_Sky_Poor <katovano2705@gmail.com>
2022-12-05 15:54:50 +0300

    add tests for footer buttons

--- test_main_page.py
+++ test_main_page.py
@@ -25,7 +25,7 @@ def test_display_logo_ExLab(browser):
     page.open()
 
 
-@allure.feature("header button")
+@allure.feature("HEADER buttons")
 def test_about_us_header(browser):
     """Test 'About Us' button in HEADER. [3] test from check-list"""
     page = MainPage(browser, MainPageLocators.LINK)
@@ -44,7 +44,7 @@ def test_about_us_header(browser):
         page.checking_about_us_anchor_after_shifting()
 
 
-@allure.feature("header button")
+@allure.feature("HEADER buttons")
 def test_projects_header(browser):
     """Test 'Projects' button in HEADER. [4] test from check-list"""
     page = MainPage(browser, MainPageLocators.LINK)
@@ -63,7 +63,7 @@ def test_projects_header(browser):
         page.checking_projects_anchor_after_shifting()
 
 
-@allure.feature("header button")
+@allure.feature("HEADER buttons")
 def test_mentors_header(browser):
     """Test 'Mentors' button in HEADER. [5] test from check-list"""
     page = MainPage(browser, MainPageLocators.LINK)
@@ -82,7 +82,7 @@ def test_mentors_header(browser):
         page.checking_mentors_anchor_after_shifting()
 
 
-@allure.feature("header button")
+@allure.feature("HEADER buttons")
 @pytest.mark.skip(reason='Button behaves strangely')
 def test_startup_for_header(browser):
     """Test 'StartUp for' button in HEADER. [6] test from check-list"""
@@ -102,7 +102,7 @@ def test_startup_for_header(browser):
         page.checking_startup_for_anchor_after_shifting()
 
 
-@allure.feature("header button")
+@allure.feature("HEADER buttons")
 def test_sun_icon_header(browser):
     """Test 'Sun icon' button in HEADER. [7] test from check-list"""
     page = MainPage(browser, MainPageLocators.LINK)
@@ -120,8 +120,8 @@ def test_sun_icon_header(browser):
     with allure.step("Step 4. Checking light theme on"):
         page.checking_light_mode_on()
     
-    
-@allure.feature("header button")
+
+@allure.feature("HEADER buttons")
 def test_join_header(browser):
     """Test 'Join' button in HEADER. [9] test from check-list"""
     page = MainPage(browser, MainPageLocators.LINK)
@@ -167,7 +167,93 @@ def test_text_your_opportunity_block(browser):
         page.checking_text_under_main_title_in_your_opportunity_block_is_present_and_displayed()
 
 
+@pytest.mark.skip(reason="Linkedin works stangely")
+@allure.feature('FOOTER buttons')
+def test_linkedin_footer(browser):
+    """Test 'Linkedin' button in FOOTER. [38] test from check-list"""
+    page = MainPage(browser, MainPageLocators.LINK)
+    page.open()
 
+    with allure.step("Step 1. Checking landing URL"):
+        page.checking_landing_url()
+
+    with allure.step("Step 2. Scrolling to FOOTER block"):
+        page.scroll_to_footer()
+
+    with allure.step("Step 3. Checking if 'Linkedin' button in FOOTER is present and displayed"):
+        page.checking_linkedin_footer_is_present_and_displayed()
+
+    with allure.step("Step 4. Click on 'Linkedin' in FOOTER"):
+        page.click_on_linkedin_footer()
+
+    with allure.step("Step 5. Checking URL opened page"):
+        page.checking_url_after_click_on_linkedin_footer()
+
+
+@allure.feature('FOOTER buttons')
+def test_instagram_footer(browser):
+    """Test 'Instagram' button in FOOTER. [39] test from check-list"""
+    page = MainPage(browser, MainPageLocators.LINK)
+    page.open()
+
+    with allure.step("Step 1. Checking landing URL"):
+        page.checking_landing_url()
+
+    with allure.step("Step 2. Scrolling to FOOTER block"):
+        page.scroll_to_footer()
+
+    with allure.step("Step 3. Checking if 'Instagram' button in FOOTER is present and displayed"):
+        page.checking_instagram_footer_is_present_and_displayed()
+
+    with allure.step("Step 4. Click on 'Instagram' in FOOTER"):
+        page.click_on_instagram_footer()
+
+    with allure.step("Step 5. Checking URL opened page"):
+        page.checking_url_after_click_on_instagram_footer()    
+
+
+@allure.feature('FOOTER buttons')
+def test_telegram_footer(browser):
+    """Test 'Telegram' button in FOOTER. [40] test from check-list"""
+    page = MainPage(browser, MainPageLocators.LINK)
+    page.open()
+
+    with allure.step("Step 1. Checking landing URL"):
+        page.checking_landing_url()
+
+    with allure.step("Step 2. Scrolling to FOOTER block"):
+        page.scroll_to_footer()
+
+    with allure.step("Step 3. Checking if 'Telegram' button in FOOTER is present and displayed"):
+        page.checking_telegram_footer_is_present_and_displayed()
+
+    with allure.step("Step 4. Click on 'Telegram' in FOOTER"):
+        page.click_on_telegram_footer()
+
+    with allure.step("Step 5. Checking URL opened page"):
+        page.checking_url_after_click_on_telegram_footer()    
+
+
+@allure.feature('FOOTER buttons')
+def test_youtube_footer(browser):
+    """Test 'Youtube' button in FOOTER. [40] test from check-list"""
+    page = MainPage(browser, MainPageLocators.LINK)
+    page.open()
+
+    with allure.step("Step 1. Checking landing URL"):
+        page.checking_landing_url()
+
+    with allure.step("Step 2. Scrolling to FOOTER block"):
+        page.scroll_to_footer()
+
+    with allure.step("Step 3. Checking if 'Youtube' button in FOOTER is present and displayed"):
+        page.checking_youtube_footer_is_present_and_displayed()
+
+    with allure.step("Step 4. Click on 'Youtube' in FOOTER"):
+        page.click_on_youtube_footer()
+
+    with allure.step("Step 5. Checking URL opened page"):
+        page.checking_url_after_click_on_telegram_footer()    
 
 
 
