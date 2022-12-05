402e89ef1d94942ecc4030418b82e3e1599e0bfe
Jean_Sky_Poor <katovano2705@gmail.com>
2022-12-05 19:29:32 +0300

    add attach_screenshot to checking defs

--- pages/main_page.py
+++ pages/main_page.py
@@ -25,9 +25,11 @@ class MainPage(BasePage):
     def checking_about_us_header_is_present_and_displayed(self):
         """Checking 'About Us' button in HEADER is present and displayed"""
         if self.is_element_present(MainPageLocators.ABOUT_US_HEADER_BUTTON) != True:
+            self.attach_screenshot('About_Us_button_in_HEADER_is_not_found')
             raise AssertionError("About Us button in HEADER is not found")
 
         if self.is_element_displayed(MainPageLocators.ABOUT_US_HEADER_BUTTON) != True:
+            self.attach_screenshot('About_Us_button_in_HEADER_is_not_displayed')
             raise AssertionError("About Us button in HEADER is not displayed")    
 
     def click_on_about_us_header(self):
@@ -38,9 +40,11 @@ class MainPage(BasePage):
     def checking_projects_header_is_present_and_displayed(self):
         """Checking Project button in HEADER is present and displayed"""
         if self.is_element_present(MainPageLocators.PROJECTS_HEADER_BUTTON) != True:
+            self.attach_screenshot('Projects_button_in_HEADER_is_not_found')
             raise AssertionError("Projects button in HEADER is not found")
 
         if self.is_element_displayed(MainPageLocators.PROJECTS_HEADER_BUTTON) != True:
+            self.attach_screenshot('Project_button_in_HEADER_is_not_displayed')
             raise AssertionError("Projects button in HEADER is not displayed")
 
     def click_on_projects_header(self):
@@ -51,9 +55,11 @@ class MainPage(BasePage):
     def checking_mentors_header_is_present_and_displayed(self):
         """Checking 'Mentors' button in HEADER is present and displayed"""
         if self.is_element_present(MainPageLocators.MENTORS_HEADER_BUTTON) != True:
+            self.attach_screenshot('Mentors_button_in_HEADER_is_not_found')
             raise AssertionError("Mentors button in HEADER is not found")
 
         if self.is_element_displayed(MainPageLocators.MENTORS_HEADER_BUTTON) != True:
+            self.attach_screenshot('Mentors_button_in_HEADER_is_not_displayed')
             raise AssertionError("Mentors button in HEADER is not displayed")
 
     def click_on_mentors_header(self):
@@ -64,9 +70,11 @@ class MainPage(BasePage):
     def checking_startup_for_header_is_present_and_displayed(self):
         """Checking 'StartUp for' button in HEADER is present and displayed"""
         if self.is_element_present(MainPageLocators.STARTUP_FOR_HEADER_BUTTON) != True:
+            self.attach_screenshot('Startup_for_button_in_HEADER_is_not_found')
             raise AssertionError("StartUp for button in HEADER is not found")
 
         if self.is_element_displayed(MainPageLocators.STARTUP_FOR_HEADER_BUTTON) != True:
+            self.attach_screenshot('Startup_for_button_in_HEADER_is_not_displayed')
             raise AssertionError("StartUp for button in HEADER is not displayed")
 
     def click_on_startup_for_header(self):
@@ -77,9 +85,11 @@ class MainPage(BasePage):
     def checking_sun_icon_header_is_present_and_displayed(self):
         """Checking 'Sun icon' button in HEADER is present and displayed"""
         if self.is_element_present(MainPageLocators.SUN_ICON_HEADER_BUTTON) != True:
+            self.attach_screenshot('Sun_icon_in_HEADER_is_not_found')
             raise AssertionError("Sun icon button in HEADER is not found")
 
         if self.is_element_displayed(MainPageLocators.SUN_ICON_HEADER_BUTTON) != True:
+            self.attach_screenshot('Sun_icon_button_in_HEADER_is_not_displayed')
             raise AssertionError("Sun icon button in HEADER is not displayed")
 
     def click_on_sun_icon_header(self):
@@ -90,9 +100,11 @@ class MainPage(BasePage):
     def checking_join_header_is_present_and_displayed(self):
         """Checking 'Join' button in HEADER is present and displayed"""
         if self.is_element_present(MainPageLocators.JOIN_HEADER_BUTTON) != True:
+            self.attach_screenshot('Join_button_in_HEADER_is_not_found')
             raise AssertionError("Join button in HEADER is not found")
 
         if self.is_element_displayed(MainPageLocators.JOIN_HEADER_BUTTON) != True:
+            self.attach_screenshot('Join_button_in_HEADER_is_not_displayed')
             raise AssertionError("Join button in HEADER is not displayed")
 
     def click_on_join_header(self):
@@ -109,25 +121,31 @@ class MainPage(BasePage):
     def checking_logo_in_your_opportunity_block_is_present_and_displayed(self):
         """Checking Logo in block YOUR OPPORTUNITY is present and displayed"""
         if self.is_element_present(MainPageLocators.LOGO_YOUR_OPPORTUNITY) != True:
+            self.attach_screenshot('Logo_in_your_opportunity_block_is_not_found')
             raise AssertionError("Logo in block YOUR OPPORTUNITY is not found")
 
         if self.is_element_displayed(MainPageLocators.LOGO_YOUR_OPPORTUNITY) != True:
+            self.attach_screenshot('Logo_in_your_opportunity_block_is_not_displayed')
             raise AssertionError("Logo in block YOUR OPPORTUNITY is not displayed")
 
     def checking_main_title_in_your_opportunity_block_is_present_and_displayed(self):
         """Checking main title in your opportynity block is present and displayed"""
         if self.is_element_present(MainPageLocators.MAIN_TITLE_YOUR_OPPROTUNITY) != True:
+            self.attach_screenshot('Main_title_in_your_opportunity_block_is_not_found')
             raise AssertionError("Main title in block YOUR OPPORTUNITY is not found")
 
         if self.is_element_displayed(MainPageLocators.MAIN_TITLE_YOUR_OPPROTUNITY) != True:
+            self.attach_screenshot('Main_title_in_your_opportunity_block_is_not_displayed')
             raise AssertionError("Main title in block YOUR OPPORTUNITY is not displayed")
 
     def checking_text_under_main_title_in_your_opportunity_block_is_present_and_displayed(self):
         """Checking text under main title in your opportunity block is present and displayed"""
         if self.is_element_present(MainPageLocators.BLOCK_TEXT_YOUR_OPPORTUNITY) != True:
+            self.attach_screenshot('Text_under_Main_title_in_your_opportunity_block_is_not_found')
             raise AssertionError("Text under Main title in block YOUR OPPORTUNITY is not found")
 
         if self.is_element_displayed(MainPageLocators.BLOCK_TEXT_YOUR_OPPORTUNITY) != True:
+            self.attach_screenshot('Text_under_Main_title_in_your_opportunity_block_is_not_displayed')
             raise AssertionError("Text under Main title in block YOUR OPPORTUNITY is not displayed")
 
 
@@ -196,9 +214,11 @@ class MainPage(BasePage):
     def checking_linkedin_footer_is_present_and_displayed(self):
         """Checking 'Linkedin' button in FOOTER is present and displayed"""
         if self.is_element_present(MainPageLocators.LINKEDIN_FOOTER) != True:
+            self.attach_screenshot('Linkedin_button_in_FOOTER_block_is_not_found')
             raise AssertionError('Linkedin button in FOOTER block is not found')
 
         if self.is_element_displayed(MainPageLocators.LINKEDIN_FOOTER) != True:
+            self.attach_screenshot('Linkedin_button_in_FOOTER_block_is_not_displayed')
             raise AssertionError('Linkedin button in FOOTER block is not displayed')
 
     def click_on_linkedin_footer(self):
@@ -215,9 +235,11 @@ class MainPage(BasePage):
     def checking_instagram_footer_is_present_and_displayed(self):
         """Checking 'Instagram' button in FOOTER is present and displayed"""
         if self.is_element_present(MainPageLocators.INSTAGRAM_FOOTER) != True:
+            self.attach_screenshot('Instagram_button_in_FOOTER_block_is_not_found')
             raise AssertionError('Instagram button in FOOTER block is not found')
 
         if self.is_element_displayed(MainPageLocators.INSTAGRAM_FOOTER) != True:
+            self.attach_screenshot('Instagram_button_in_FOOTER_block_is_not_displayed')
             raise AssertionError('Instagram button in FOOTER block is not displayed')
 
     def click_on_instagram_footer(self):
@@ -234,9 +256,11 @@ class MainPage(BasePage):
     def checking_telegram_footer_is_present_and_displayed(self):
         """Checking 'Telegram' button in FOOTER is present and displayed"""
         if self.is_element_present(MainPageLocators.TELEGRAM_FOOTER) != True:
+            self.attach_screenshot('Telegram_button_in_FOOTER_block_is_not_found')
             raise AssertionError('Telegram button in FOOTER block is not found')
 
         if self.is_element_displayed(MainPageLocators.TELEGRAM_FOOTER) != True:
+            self.attach_screenshot('Telegram_button_in_FOOTER_block_is_not_displayed')
             raise AssertionError('Telegram button in FOOTER block is not displayed')
 
     def click_on_telegram_footer(self):
@@ -253,9 +277,11 @@ class MainPage(BasePage):
     def checking_youtube_footer_is_present_and_displayed(self):
         """Checking 'Youtube' button in FOOTER is present and displayed"""
         if self.is_element_present(MainPageLocators.YOUTUBE_FOOTER) != True:
+            self.attach_screenshot('Youtube_button_in_FOOTER_block_is_not_found')
             raise AssertionError('Youtube button in FOOTER block is not found')
 
         if self.is_element_displayed(MainPageLocators.YOUTUBE_FOOTER) != True:
+            self.attach_screenshot('Youtube_button_in_FOOTER_block_is_not_displayed')
             raise AssertionError('Youtube button in FOOTER block is not displayed')
 
     def click_on_youtube_footer(self):
