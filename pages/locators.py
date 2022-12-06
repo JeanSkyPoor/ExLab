19a63c7051e300f7089e6f679272007844b9f47a
Jean_Sky_Poor <katovano2705@gmail.com>
2022-12-07 00:21:08 +0300

    drop some trash

--- pages/locators.py
+++ pages/locators.py
@@ -48,28 +48,3 @@ class MainPageLocators():
     #block HELP PROJECT
     BOOSTY_HELP_PROJECT_BUTTON = (By.CSS_SELECTOR, 'div.sc-bWXABl.klepWn > a:nth-of-type(1)')
     PATREON_HELP_PROJECT_BUTTON = (By.CSS_SELECTOR, 'div.sc-bWXABl.klepWn > a:nth-of-type(2)')
-    # BACKGROUND_FOR_THEME = (By.CSS_SELECTOR, '#root > div')
-
-    
-    # LIGHT_ICON_WHILE_DARK_MODE = (By.CSS_SELECTOR, 'div.sc-fnykZs.fEkGUM')
-    # LOGO_ICON = (By.CSS_SELECTOR, 'div.sc-gKXOVf >div.sc-jqUVSM.EnPDN')
-    # ABOUT_US_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(1)')
-    # PROJECTS_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(2)')
-    # MENTORS_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(3)')
-    # STARTUP_FOR_BUTTON_HEADER = (By.CSS_SELECTOR, 'li.sc-crXcEl:nth-of-type(4)')
-    # JOIN_BUTTON_HEADER = (By.CSS_SELECTOR, 'div.sc-hAZoDl.hrEelO')
-    # SUN_ICON = (By.CSS_SELECTOR, 'div.sc-fnykZs.fEkGUM')
-    
-        
-
-   # #Block YOUR OPPORTUNITY
-    # LOGO_GIF = (By.CSS_SELECTOR, 'div.sc-idiyUo.jKIKBm > img')
-    # YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-kgflAQ.gupdxc')
-    # TEXT_UNDER_YOUR_OPPORTUNITY = (By.CSS_SELECTOR, 'div.sc-bBrHrO.lmeoyY')
-
-    # #Block ABOUT US
-    # ABOUT_US_MAIN = (By.CSS_SELECTOR, '#about > div.sc-eCYdqJ.koNCEH.is-inview')
-    # ABOUT_US_BLOCK_TEXT = (By.CSS_SELECTOR, 'p.sc-cCsOjp.cdaqyF')
-    # WHY_EXLAB_MAIN = (By.CSS_SELECTOR, 'div.sc-bZnhIo.CLhmH.is-inview')
-    # WHY_EXLAB_BLOCK_TEXT = (By.CSS_SELECTOR, 'ol.sc-iAvgwm.fQGFrP.is-inview')
-    # WHY_EXLAB_JOIN = (By.CSS_SELECTOR, 'a.sc-dkzDqf.gpYSxm.is-inview')
