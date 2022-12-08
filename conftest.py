eca77e79f8e7c5e065677eb805370c4577bd46cb
Jean_Sky_Poor <katovano2705@gmail.com>
2022-12-08 22:58:18 +0300

    on headless

--- conftest.py
+++ conftest.py
@@ -13,7 +13,7 @@ def pytest_addoption(parser):
 
     parser.addoption('--language', action='store', default='en',
                      help="Choose language: 'ru' or 'en'")
-    parser.addoption('--headless', action='store', default='None',
+    parser.addoption('--headless', action='store', default='true',
                      help="Open a browser invisible, without GUI is used by default")
 
 @pytest.fixture(scope="function")
