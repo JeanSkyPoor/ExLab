d4d55388f4ff52674b81282d22687aff22cacb20
Jean_Sky_Poor <katovano2705@gmail.com>
2023-02-03 05:10:14 +0300

    small renames

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -3,7 +3,7 @@ import allure
 from pages.landing_page import LandingPage
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_black_theme_on_by_default(browser):
     """Test landing URL and black theme on default. [1] test from check-list"""
 
@@ -11,7 +11,7 @@ def test_black_theme_on_by_default(browser):
         checking_dark_mode_on()
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_logo_exlab_header(browser):
     """Test logo ExLab in HEADER. [2] test from check-list. Without refresh method in test"""
 
@@ -20,7 +20,7 @@ def test_expected_result_logo_exlab_header(browser):
     #TO DO: доделать, когда добавят функцию рефреша страницы
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_about_us_header(browser):
     """Test 'About_Us' button in HEADER. [3] test from check-list"""
 
@@ -28,15 +28,16 @@ def test_expected_result_about_us_header(browser):
         click_on_about_us_header()
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_projects_header(browser):
     """Test 'Projects' button in HEADER. [4] test from check-list"""
 
     LandingPage(browser).\
         click_on_projects_header()
 
+
 @pytest.mark.skip(reason="Auto-Scroll to mentor's block works wrong")
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_mentors_header(browser):
     """Test 'Mentors' button in HEADER. [5] test from check-list"""
 
@@ -45,7 +46,7 @@ def test_expected_result_mentors_header(browser):
 
 
 @pytest.mark.skip(reason="Auto-Scroll to StartUp_For block works wrong")
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_startup_for_header(browser):
     """Test 'StartUp_For' button in HEADER. [6] test from check-list"""
 
@@ -53,7 +54,7 @@ def test_expected_result_startup_for_header(browser):
         click_on_startup_for_header()
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_sun_icon_header(browser):
     """Test 'Sun_Icon' button in HEADER. [7] test from check-list"""
     
@@ -61,7 +62,7 @@ def test_expected_result_sun_icon_header(browser):
         click_on_sun_icon_header()
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_join_header(browser):
     """Test 'Join' button in HEADER. [9] test from check-list"""
 
@@ -69,7 +70,7 @@ def test_expected_result_join_header(browser):
         click_on_join_header()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_logo_gif_your_opportunity_block(browser):
     """Test 'Logo_Gif' in block YOUR_OPPORTYNITY is present and displayed. [11] test from check-list"""
 
@@ -77,7 +78,7 @@ def test_logo_gif_your_opportunity_block(browser):
         checking_logo_gif_in_your_opportunity_block_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_text_your_opportunity_block(browser):
     """Test 'Main_Title' and 'Description' in YOUR OPPORTUNITY block is present and displayed. [12] test from check-list"""
 
@@ -86,7 +87,7 @@ def test_text_your_opportunity_block(browser):
         checking_description_in_your_opportunity_block_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_about_us_block(browser):
     """Test About_Us title and description is present and displayed. [13] test from check-list"""
     
@@ -96,7 +97,7 @@ def test_about_us_block(browser):
         checking_description_about_us_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_why_exlab_block(browser):
     """Test Why_Exlab title and description is present and displayed. [14] test from check-list"""
 
@@ -106,7 +107,7 @@ def test_why_exlab_block(browser):
         checking_description_why_exlab_is_present_and_displayed()
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_join_why_exlab_block(browser):
     """Test 'Join' button in Why Exlab block. [15] test from check-list"""
 
@@ -114,7 +115,7 @@ def test_join_why_exlab_block(browser):
         click_on_join_why_exlab()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_title_projects_block(browser):
     """Test 'Projects' title in Projects block is present and displayed. [16] test from check-list"""
 
@@ -123,7 +124,7 @@ def test_title_projects_block(browser):
         checking_projects_title_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_logos_and_descriptions_projects_block(browser):
     """Test logos and descriptions of projects in Projects block are present and displayed. [17] test from check-list"""
 
@@ -132,7 +133,7 @@ def test_logos_and_descriptions_projects_block(browser):
         checking_logos_and_descriptions_projects_block()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_title_mentors_block(browser):
     """Test Mentors_Title in Mentors block are present and displayed. [18] test from check-list"""
 
@@ -141,7 +142,7 @@ def test_title_mentors_block(browser):
         checking_title_mentors_is_present_and_displayed()
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_mentors_spoilers(browser):
     """Test spoilers of mentors in mentors block. [20] test from check-list"""
 
@@ -150,7 +151,7 @@ def test_mentors_spoilers(browser):
         check_all_mentors_spoilers()
         #To Do: подумать об объединении 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_title_startup_for_block(browser):
     """Test StartUp_For_Title in StartUp_For block are present and displayed. [24] test from check-list"""
 
@@ -159,7 +160,7 @@ def test_title_startup_for_block(browser):
         checking_title_startup_for_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_descriptions_startup_for_block(browser):
     """Test Descriptions in StartUp_For block are present and displayed. [28] test from check-list"""
 
@@ -168,7 +169,7 @@ def test_descriptions_startup_for_block(browser):
         checking_descriptions_startup_for_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_title_help_project_block(browser):
     """Test Help_Project_Title in Help_Project block are present and displayed. [30] test from check-list"""
 
@@ -177,7 +178,7 @@ def test_title_help_project_block(browser):
         checking_title_help_project_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_description_help_project_block(browser):
     """Test Description in Help_Project block are present and displayed. [31] test from check-list"""
 
@@ -186,7 +187,7 @@ def test_description_help_project_block(browser):
         checking_description_help_project_is_present_and_displayed()
     
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_boosty_button_help_project(browser):
     """Test Boosty button in Help Project block. [32] test from check-list"""
 
@@ -194,7 +195,7 @@ def test_expected_result_boosty_button_help_project(browser):
         click_on_boosty_help_project()
 
 
-@allure.feature("Functional")
+@allure.feature("functional")
 def test_expected_result_patreon_button_help_project(browser):
     """Test Patreon button in Help Project block. [33] test from check-list. Without Patreon page checking"""
     
@@ -204,7 +205,7 @@ def test_expected_result_patreon_button_help_project(browser):
         #To Do: написать реализацию кнопки Patreon, когда добавят функционал
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_title_stay_connected_block(browser):
     """Test Title_Stay_Connected in FOOTER block are present and displayed. [34] test from check-list"""
 
@@ -213,7 +214,7 @@ def test_title_stay_connected_block(browser):
         checking_title_stay_connected_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_description_stay_connected_block(browser):
     """Test description in FOOTER block are present and displayed. [35] test from check-list"""
 
@@ -222,7 +223,7 @@ def test_description_stay_connected_block(browser):
         checking_description_stay_connected_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_logo_footer_block(browser):
     """Test Logo in FOOTER block are present and displayed. [36] test from check-list"""
 
@@ -231,7 +232,7 @@ def test_logo_footer_block(browser):
         checking_logo_footer_is_present_and_displayed()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_text_under_logo_footer_block(browser):
     """Test Text_Under_Logo in FOOTER block are present and displayed. [37] test from check-list"""
 
@@ -240,7 +241,7 @@ def test_text_under_logo_footer_block(browser):
         checking_text_under_logo_footer_is_present_and_displayed()
 
 
-@allure.feature('Functional')
+@allure.feature('functional')
 def test_expected_result_linkedin_footer(browser):
     """Test 'Linkedin' button in FOOTER. [38] test from check-list"""
 
@@ -248,7 +249,7 @@ def test_expected_result_linkedin_footer(browser):
         click_on_linkedin_footer()
 
 
-@allure.feature('Functional')
+@allure.feature('functional')
 def test_expected_result_instagram_footer(browser):
     """Test 'Instagram' button in FOOTER. [39] test from check-list"""
 
@@ -256,7 +257,7 @@ def test_expected_result_instagram_footer(browser):
         click_on_instagram_footer()
 
 
-@allure.feature('Functional')
+@allure.feature('functional')
 def test_expected_result_telegram_footer(browser):
     """Test 'Telegram' button in FOOTER. [40] test from check-list"""
 
@@ -264,7 +265,7 @@ def test_expected_result_telegram_footer(browser):
         click_on_telegram_footer()
 
 
-@allure.feature('Functional')
+@allure.feature('functional')
 def test_expected_result_youtube_footer(browser):
     """Test 'Youtube' button in FOOTER. [41] test from check-list"""
 
@@ -272,7 +273,7 @@ def test_expected_result_youtube_footer(browser):
         click_on_youtube_footer()
 
 
-@allure.feature("Unfunctional")
+@allure.feature("non-functional")
 def test_mail_address_footer_block(browser):
     """Test Mail_Address in FOOTER block are present and displayed. [42] test from check-list"""
 
